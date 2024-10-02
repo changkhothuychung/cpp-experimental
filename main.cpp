b6e43e355e919c911ee238c669433f90ae9bf823
changkhothuychung <nhat7203@gmail.com>
2024-10-01 23:26:51 -0400

    update tuple simple implementation

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <type_traits>
+#include "src/tuple.h"
 
 
 int main()
 {
+    nhat::tuple<bool, int, float> tuple;
+    tuple.get<2>() = 2.0f;
+    std::cout << tuple.get<2>() << std::endl; 
     return 0;
 }
--- src/tuple.h
+++ src/tuple.h
@@ -1,23 +1,70 @@
 #ifndef NHAT_TUPLE 
 #define NHAT_TUPLE 
 
+#include <type_traits>
 
 namespace nhat 
 {
+
+    static constexpr int maximum(int a, int b)
+    {
+        if (a > b)
+        {
+            return a; 
+        }
+        return b; 
+    }
+
+    template<int Idx, typename A = void, typename... Args>
+    struct ArgType
+    {
+        using type = typename std::conditional<Idx == 0, A, typename ArgType<maximum(Idx - 1, 0), Args...>::type>::type; 
+
+    };
+
+    template<typename T>
+    struct ArgType<0, T>
+    {
+        using type = T;
+    };
+
     struct tuple_leaf; 
 
     struct member_end{}; 
 
-    template<int Idx, typename Member = void, typename... Membs>
+    template<int Idx, typename M = void, typename... Membs>
     struct tuple_impl
     {
-
+        M member; 
+        typename std::conditional<sizeof...(Membs) == 0, member_end, tuple_impl<Idx+1, Membs...>>::type next_member; 
+        static constexpr int getIndex()
+        {
+            return Idx; 
+        }
     };
 
     template<typename... Args>
     struct tuple
     {
+        tuple_impl<0, Args...> members; 
+        template<int Idx, typename Ret, typename T>
+        Ret& getMemberFromHolder(T& holder)
+        {
+            if constexpr (T::getIndex() == Idx)
+            {
+                return holder.member; 
+            }
+            else
+            {
+                return getMemberFromHolder<Idx, Ret>(holder.next_member); 
+            }
+        }
 
+        template<int Idx>
+        ArgType<Idx, Args...>::type& get()
+        {
+            return getMemberFromHolder<Idx, typename ArgType<Idx, Args...>::type>(members); 
+        }
     };
 };
 
