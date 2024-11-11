2666ccbc6a9cae5eacc20e9a233232c98fb2f80f
changkhothuychung <nhat7203@gmail.com>
2024-11-11 01:27:52 -0500

    working on get<>

--- src/tuple.h
+++ src/tuple.h
@@ -27,6 +27,39 @@ namespace nhat
         }
     }; 
 
+    template<size_t N, typename T, typename... Ts> 
+    struct get_nth: get_nth<N - 1, Ts...> 
+    {
+        static constexpr auto get(tuple<T, Ts...>& t)
+        {
+            if constexpr (N == 0)
+            {
+                return t.value; 
+            }
+            else
+            {
+                return get_nth<N-1, Ts...>::get(t.rest); 
+            }
+        }
+    };
+
+    template<typename T, typename... Ts> 
+    struct get_nth<0, T, Ts...>
+    {
+        using type = T; 
+
+        static constexpr T& get(tuple<T, Ts...>& t)
+        {   
+            return t.value;
+        }
+    };
+
+    template<size_t I, typename... Ts> 
+    typename get_nth<I, Ts...>::type& get(tuple<Ts...>& t)
+    {
+        return get_nth<I, Ts...>::get(t); 
+    }
+
 
 };
 
